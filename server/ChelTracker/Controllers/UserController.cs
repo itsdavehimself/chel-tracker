b80f8b887e26a707500a5d8c44dea837f4d1592d
David <23064280+itsdavehimself@users.noreply.github.com>
2024-05-15 16:10:34 -0500

    Create User controller classes

--- server/ChelTracker/Controllers/UserController.cs
+++ server/ChelTracker/Controllers/UserController.cs
@@ -2,11 +2,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ChelTracker.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ChelTracker.Controllers
 {
-    public class UserController
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
     {
+        private readonly ApplicationDBContext _context;
+        public UserController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
 
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _context.Users.ToList();
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var user = _context.Users.Find(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
     }
 }
