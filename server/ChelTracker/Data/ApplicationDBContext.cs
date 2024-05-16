3f32f62902618c0cebc1d8e618f588f3ae63b7aa
David <23064280+itsdavehimself@users.noreply.github.com>
2024-05-16 09:46:23 -0500

    Remove cascade behavior

--- server/ChelTracker/Data/ApplicationDBContext.cs
+++ server/ChelTracker/Data/ApplicationDBContext.cs
@@ -16,20 +16,5 @@ namespace ChelTracker.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Game> Games { get; set; }
         public DbSet<Opponent> Opponents { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Game>()
-                .HasOne(g => g.User)
-                .WithMany(u => u.Games)
-                .HasForeignKey(g => g.UserId)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            modelBuilder.Entity<Game>()
-                .HasOne(g => g.Opponent)
-                .WithMany()
-                .HasForeignKey(g => g.OpponentId)
-                .OnDelete(DeleteBehavior.Restrict);
-        }
     }
 }
