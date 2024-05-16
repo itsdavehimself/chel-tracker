7597009c1e9f399caa77fd24bafb0b41c8cf3357
David <23064280+itsdavehimself@users.noreply.github.com>
2024-05-15 19:22:51 -0500

    Cascade delete game when user delete only

--- server/ChelTracker/Data/ApplicationDBContext.cs
+++ server/ChelTracker/Data/ApplicationDBContext.cs
@@ -9,13 +9,27 @@ namespace ChelTracker.Data
 {
     public class ApplicationDBContext : DbContext
     {
-        public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
         {
-
         }
 
         public DbSet<User> Users { get; set; }
         public DbSet<Game> Games { get; set; }
         public DbSet<Opponent> Opponents { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Game>()
+                .HasOne(g => g.User)
+                .WithMany(u => u.Games)
+                .HasForeignKey(g => g.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Game>()
+                .HasOne(g => g.Opponent)
+                .WithMany()
+                .HasForeignKey(g => g.OpponentId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
