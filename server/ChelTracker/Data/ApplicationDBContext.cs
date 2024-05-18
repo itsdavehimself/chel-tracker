33a97fa9a24dbfa3317fbd1e4987cf2534a11d06
David <23064280+itsdavehimself@users.noreply.github.com>
2024-05-17 19:26:26 -0500

    Create identity roles for users

--- server/ChelTracker/Data/ApplicationDBContext.cs
+++ server/ChelTracker/Data/ApplicationDBContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ChelTracker.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,8 +15,27 @@ namespace ChelTracker.Data
         {
         }
 
-        // public DbSet<User> Users { get; set; }
         public DbSet<Game> Games { get; set; }
         public DbSet<Opponent> Opponents { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "USER"
+                },
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
