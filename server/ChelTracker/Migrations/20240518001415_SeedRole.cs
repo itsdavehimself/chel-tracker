479b315930e8d41ba68eec1534107144ca38e5b1
David <23064280+itsdavehimself@users.noreply.github.com>
2024-05-17 19:25:57 -0500

    Seed roles in database

---
+++ server/ChelTracker/Migrations/20240518001415_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ChelTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "2507e3d2-75f1-424f-8aeb-808e8992cfb2", null, "User", "USER" },
+                    { "2e2ea7c5-0a63-42df-be76-80be35716bef", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2507e3d2-75f1-424f-8aeb-808e8992cfb2");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2e2ea7c5-0a63-42df-be76-80be35716bef");
+        }
+    }
+}
