9cfa360e370fca4ab49322debc22d6347e915618
David <23064280+itsdavehimself@users.noreply.github.com>
2024-05-21 14:50:15 -0500

    Implement login api call

--- client/src/containers/Login/Login.tsx
+++ client/src/containers/Login/Login.tsx
@@ -2,23 +2,36 @@ import AuthBtn from '../../components/AuthBtn/AuthBtn';
 import AuthInput from '../../components/AuthInput/AuthInput';
 import styles from './Login.module.scss';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import useLogin from '../../hooks/useLogin';
 
 const Login: React.FC = () => {
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const { login } = useLogin();
+
+    const handleLogin = (e: React.FormEvent): void => {
+        e.preventDefault();
+        login(username, password);
+    };
+
     return (
         <div className={styles.auth}>
             <div className={styles['auth-container']}>
                 <h1>Welcome back to the barn, ya beauty.</h1>
-                <form className={styles['auth-form']}>
+                <form className={styles['auth-form']} onSubmit={handleLogin}>
                     <h2>Login</h2>
                     <AuthInput
                         label="Username"
                         inputIdentifier="username"
                         type="text"
+                        onChange={(e) => setUsername(e.target.value)}
                     />
                     <AuthInput
                         label="Password"
                         inputIdentifier="password"
                         type="password"
+                        onChange={(e) => setPassword(e.target.value)}
                     />
                     <div className={styles['auth-btns']}>
                         <AuthBtn label="Login" />
