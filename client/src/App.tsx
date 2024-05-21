d019249a31ea481b4892f2bdfc8d5757549b319c
David <23064280+itsdavehimself@users.noreply.github.com>
2024-05-21 11:12:25 -0500

    Setup basic routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,19 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.scss';
 import Dashboard from './containers/Dashboard/Dashboard';
+import Login from './containers/Login/Login';
 import Navbar from './containers/Navbar/Navbar';
 
 function App() {
     return (
         <>
-            <Navbar />
-            <Dashboard />
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
