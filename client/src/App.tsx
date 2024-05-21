ec0d7d4cbd743b5c0b5846bff27beb3a629b7b66
David <23064280+itsdavehimself@users.noreply.github.com>
2024-05-21 14:48:12 -0500

    Remove unused import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
 import Dashboard from './containers/Dashboard/Dashboard';
 import Login from './containers/Login/Login';
