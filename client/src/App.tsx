7ee244f01ab7622703eb1761ec25531d49839da2
David <23064280+itsdavehimself@users.noreply.github.com>
2024-05-21 16:18:30 -0500

    Redirect depending on if user signed in/out

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,16 +4,27 @@ import Dashboard from './containers/Dashboard/Dashboard';
 import Login from './containers/Login/Login';
 import Navbar from './containers/Navbar/Navbar';
 import SignUp from './containers/SignUp/SignUp';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+    const { user } = useAuthContext();
     return (
         <>
             <BrowserRouter>
                 <Navbar />
                 <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<SignUp />} />
-                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route
+                        path="/login"
+                        element={!user ? <Login /> : <Dashboard />}
+                    />
+                    <Route
+                        path="/signup"
+                        element={!user ? <SignUp /> : <Dashboard />}
+                    />
+                    <Route
+                        path="/dashboard"
+                        element={user ? <Dashboard /> : <Login />}
+                    />
                 </Routes>
             </BrowserRouter>
         </>
