0febbee038226653d527d9c5bdbb513064b7f94c
David <23064280+itsdavehimself@users.noreply.github.com>
2024-05-21 16:02:09 -0500

    Create validation & display errors

--- client/src/containers/Login/Login.tsx
+++ client/src/containers/Login/Login.tsx
@@ -7,11 +7,28 @@ import useLogin from '../../hooks/useLogin';
 
 const Login: React.FC = () => {
     const [username, setUsername] = useState<string>('');
+    const [usernameError, setUsernameError] = useState<boolean>(false);
     const [password, setPassword] = useState<string>('');
-    const { login } = useLogin();
+    const [passwordError, setPasswordError] = useState<boolean>(false);
+    const { login, isLoading, error } = useLogin();
+
+    const checkUsername = () => {
+        setUsernameError(username === '');
+    };
+
+    const checkPassword = () => {
+        setPasswordError(password === '');
+    };
 
     const handleLogin = (e: React.FormEvent): void => {
         e.preventDefault();
+        setUsernameError(false);
+        setPasswordError(false);
+        if (!username || !password) {
+            checkUsername();
+            checkPassword();
+            return;
+        }
         login(username, password);
     };
 
@@ -26,16 +43,26 @@ const Login: React.FC = () => {
                         inputIdentifier="username"
                         type="text"
                         onChange={(e) => setUsername(e.target.value)}
+                        error={usernameError}
                     />
                     <AuthInput
                         label="Password"
                         inputIdentifier="password"
                         type="password"
                         onChange={(e) => setPassword(e.target.value)}
+                        error={passwordError}
                     />
                     <div className={styles['auth-btns']}>
-                        <AuthBtn label="Login" />
+                        <AuthBtn label="Login" isLoading={isLoading} />
                     </div>
+                    <div className={styles.error}>
+                        {error && !usernameError && !passwordError
+                            ? error.message
+                            : usernameError || passwordError
+                              ? 'Please fill out all fields.'
+                              : ''}
+                    </div>
+
                     <div className={styles['signup-container']}>
                         Still have all your teeth?{' '}
                         <Link to="/signup">Sign up</Link>
