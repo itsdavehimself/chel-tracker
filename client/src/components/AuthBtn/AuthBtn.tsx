f47171775b36169f88d2328901aef6bce2e98643
David <23064280+itsdavehimself@users.noreply.github.com>
2024-05-21 16:19:02 -0500

    Add loading spinner state

--- client/src/components/AuthBtn/AuthBtn.tsx
+++ client/src/components/AuthBtn/AuthBtn.tsx
@@ -2,10 +2,19 @@ import styles from './AuthBtn.module.scss';
 
 interface AuthBtnProps {
     label: string;
+    isLoading: boolean;
 }
 
-const AuthBtn: React.FC<AuthBtnProps> = ({ label }) => {
-    return <button className={styles['auth-btn']}>{label}</button>;
+const AuthBtn: React.FC<AuthBtnProps> = ({ label, isLoading }) => {
+    return (
+        <button className={styles['auth-btn']} disabled={isLoading}>
+            {isLoading ? (
+                <span className={styles['loading-spinner']}></span>
+            ) : (
+                label
+            )}
+        </button>
+    );
 };
 
 export default AuthBtn;
