adc96de771a02d141f412a0dd2401260cd104f96
David <23064280+itsdavehimself@users.noreply.github.com>
2024-05-21 12:20:25 -0500

    Add Sign Up route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import './App.scss';
 import Dashboard from './containers/Dashboard/Dashboard';
 import Login from './containers/Login/Login';
 import Navbar from './containers/Navbar/Navbar';
+import SignUp from './containers/SignUp/SignUp';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Navbar />
                 <Routes>
                     <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<SignUp />} />
                     <Route path="/dashboard" element={<Dashboard />} />
                 </Routes>
             </BrowserRouter>
